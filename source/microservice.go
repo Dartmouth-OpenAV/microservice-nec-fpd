56684be29834a3196d89616425163a169f1c1b44
markjernf <mark.j.franklin@dartmouth.edu>
2025-05-07 17:40:03 -0400

    Adding return value for M001

--- source/microservice.go
+++ source/microservice.go
@@ -2,39 +2,41 @@ package main
 
 import (
 	"errors"
+
 	"github.com/Dartmouth-OpenAV/microservice-framework/framework"
 )
- 
-	//deviceStates[socketKey]["videomute" + output] = `"false"`  // unmuted
+
+//deviceStates[socketKey]["videomute" + output] = `"false"`  // unmuted
 
 func setFrameworkGlobals() {
 	// Define device specific globals
-	framework.DefaultSocketPort = 7142   // Sony Bravia API's default socket port
+	framework.DefaultSocketPort = 7142 // Sony Bravia API's default socket port
 	framework.CheckFunctionAppendBehavior = "Remove older instance"
 	framework.MicroserviceName = "OpenAV NEC FPD Microservice"
 
-	// Bravias unmute audio when changing volume, so now we need to update the audiomute cache value so 
+	// Bravias unmute audio when changing volume, so now we need to update the audiomute cache value so
 	// our system catches up with that change quickly.  We do this by simply setting the cache value to what we know
 	// it will be.
 	framework.RelatedActions["volume"] = make(map[string]string)
 	framework.RelatedActions["volume"]["endPoint"] = "audiomute"
 	framework.RelatedActions["volume"]["value"] = `"` + "false" + `"`
-	
+
 	framework.RegisterMainGetFunc(doDeviceSpecificGet)
 	framework.RegisterMainSetFunc(doDeviceSpecificSet)
 	// At least some NEC devices can be slow to respond, so give them a little more than the default timing
 	framework.TryReadTimeout = 1000 // milliseconds
-	framework.ReadFailSleep = 2000 // milliseconds
+	framework.ReadFailSleep = 2000  // milliseconds
 }
 
 // Every microservice using this golang microservice framework needs to provide this function to invoke functions to do sets.
 // socketKey is the network connection for the framework to use to communicate with the device.
 // setting is the first parameter in the URI.
 // arg1 are the second and third parameters in the URI.
-//   Example PUT URIs that will result in this function being invoked:
-// 	 ":address/:setting/"
-//   ":address/:setting/:arg1"
-//   ":address/:setting/:arg1/:arg2"
+//
+//	  Example PUT URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
 func doDeviceSpecificSet(socketKey string, setting string, arg1 string, arg2 string, arg3 string) (string, error) {
 	function := "doDeviceSpecificSet"
 
@@ -62,17 +64,19 @@ func doDeviceSpecificSet(socketKey string, setting string, arg1 string, arg2 str
 // socketKey is the network connection for the framework to use to communicate with the device.
 // setting is the first parameter in the URI.
 // arg1 are the second and third parameters in the URI.
-//   Example GET URIs that will result in this function being invoked:
-// 	 ":address/:setting/"
-//   ":address/:setting/:arg1"
-//   ":address/:setting/:arg1/:arg2"
+//
+//	  Example GET URIs that will result in this function being invoked:
+//		 ":address/:setting/"
+//	  ":address/:setting/:arg1"
+//	  ":address/:setting/:arg1/:arg2"
+//
 // Every microservice using this golang microservice framework needs to provide this function to invoke functions to do gets.
 func doDeviceSpecificGet(socketKey string, setting string, arg1 string, arg2 string) (string, error) {
 	function := "doDeviceSpecificGet"
 
 	switch setting {
 	case "power":
-		return getPower(socketKey)       // no argument - power is global to the device
+		return getPower(socketKey) // no argument - power is global to the device
 	case "videoroute":
 		return getVideoRoute(socketKey, arg1) // arg1 is output name which isn't used because there is only one
 	case "volume":
